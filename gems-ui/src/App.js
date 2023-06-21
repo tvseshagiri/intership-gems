6d737dbbd93f0a14934acfe586cb7d7eed70d456
Seshagiri Telkapalli Venkata <tvseshagiri@gmail.com>
2023-06-21 19:24:05 +0530

    Initial Version of UI

---
+++ gems-ui/src/App.js
@@ -0,0 +1,12 @@
+export function App() {
+
+    return (
+        <>
+            <h1>Hello World</h1>
+            <button className="p-2 rounded bg-blue-500 hover:bg-red-600 transition">
+                Click Me!!!
+            </button>
+        </>
+    )
+
+}
